19494b616fc43b21a8593ef5c7b0d07da5f4f90c
Jaesang Won <77087336+wonjaesang@users.noreply.github.com>
2024-02-13 14:59:09 +0900

    Update share.js

--- js/share.js
+++ js/share.js
@@ -7,7 +7,7 @@ function setShare(){
   const shareDes = infoList[resultAlt].name;
   const shareImage = url + 'img/image-' + resultAlt + '.jpg';
   const shareURL = url + 'page/result-' + resultAlt + '.html';
-  Kakao.Share.sendDefault({
+  Kakao.Share.sendCustom({
   objectType: 'feed',
   content: {
     title: shareTitle,
